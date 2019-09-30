64e5736551b43f2a29da9a69b0b0081b7475c8d2
AndresChila <AndresChila@DESKTOP-BK0R245>
2019-09-29 20:31:48 -0500

    tests

---
+++ MVC/src/test/java/controlador/ControladorTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.awt.event.ActionEvent;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author AndresChila
+ */
+public class ControladorTest {
+    
+    public ControladorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of iniciar method, of class Controlador.
+     */
+    @Test
+    public void testIniciar() {
+        
+    }
+
+    /**
+     * Test of actionPerformed method, of class Controlador.
+     */
+    @Test
+    public void testActionPerformed() {
+        int a =2+6;
+         if (a==8){
+             System.out.print(a);
+         }
+         else{
+             
+         }
+        
+        
+    }
+    
+}
---
+++ MVC/src/test/java/modelo/ModeloTest.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author AndresChila
+ */
+public class ModeloTest {
+    
+    int numero1=2, numero2 =2;
+    public ModeloTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getNumero1 method, of class Modelo.
+     */
+    @Test
+    public void testGetNumero1() {
+       
+    }
+
+    /**
+     * Test of setNumero1 method, of class Modelo.
+     */
+    @Test
+    public void testSetNumero1() {
+        
+    }
+
+    /**
+     * Test of getNumero2 method, of class Modelo.
+     */
+    @Test
+    public void testGetNumero2() {
+        
+    }
+
+    /**
+     * Test of setNumero2 method, of class Modelo.
+     */
+    @Test
+    public void testSetNumero2() {
+       
+    }
+
+    /**
+     * Test of getResultado method, of class Modelo.
+     */
+    @Test
+    public void testGetResultado() {
+        
+    }
+
+    /**
+     * Test of setResultado method, of class Modelo.
+     */
+    @Test
+    public void testSetResultado() {
+        
+    }
+
+    /**
+     * Test of Multiplicar method, of class Modelo.
+     */
+    @Test
+    public void testMultiplicar() {
+        System.out.println("Multiplicar");
+       
+        int expResult = 4;
+        int result = 2*2;
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
